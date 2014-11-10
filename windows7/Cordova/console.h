a24d17d1997b8669bb606cf5081ba5165c30fa1f
eposjk <eposjk@users.noreply.github.com>
2014-11-10 14:17:01 +0100

    fixed copy+paste error (console.c and console.h were written by me, not by Intel!)

--- windows7/Cordova/console.h
+++ windows7/Cordova/console.h
@@ -1,5 +1,3 @@
-// Copyright 2012 Intel Corporation
-//
 // Licensed to the Apache Software Foundation (ASF) under one
 // or more contributor license agreements.  See the NOTICE file
 // distributed with this work for additional information
